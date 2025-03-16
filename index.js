bcea8b55a2e1195207eac4c0d984b292d2a1b2f1
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-03-16 19:36:26 +0300

    Updated assignment

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
@@ -25,24 +24,41 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
+// 1. Destructure sounds animals make
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// 2. Bolt the horse wandered off
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+// 3. Little the chicken went back to the coop
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+// 4. Destructure color names
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
-// Arrays
+// 5. Skip indigo
+const [r, o, y, g, b, , v] = colors;
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+// 6. Only assign indigo
+const [, , , , , indg] = colors;
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+// 7. Destructure all muppet properties
+const { muppetName, color, song, job, partner } = muppet;
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 8. Destructure songs 2 and 4, and Kermit's job and partner
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
 
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+console.log(moo, neigh, baa, oink, cluck);
+console.log(bessie, dolly, babe, little);
+console.log(blackAndWhite, black, pink);
+console.log(red, orange, yellow, green, blue, indigo, violet);
+console.log(r, o, y, g, b, v);
+console.log(indg);
+console.log(muppetName, color, song, job, partner);
+console.log(song2, song4, nestedJob, nestedPartner);
